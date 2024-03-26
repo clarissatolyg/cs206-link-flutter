66d5942de944f132fb447e3db2e071df45cf02bd
MuhammadIzzatJunaidie <muhammadizzatjunaidie@gmail.com>
2024-03-26 13:12:53 +0800

    Loading page
    added splash screen and loading icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:link_flutter/pages/onboarding_page.dart';
+import 'package:link_flutter/pages/splash.dart';
 import 'package:link_flutter/theme/color.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +21,10 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: white,
           appBarTheme: AppBarTheme(color: white, elevation: 0),
         ),
-        home: const OnBoardingPage());
+        home: AnimatedSplashScreen(
+          duration: 3000,
+          splash: 'assets/images/logo.png', 
+          pageTransitionType: PageTransitionType.fade,
+          nextScreen: OnBoardingPage(),));
   }
 }
